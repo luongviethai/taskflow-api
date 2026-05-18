52b4e45dfa94096871df737e0fa088d96b9a6b9d
VCI-HaiLV <hailv@vcijsc.com>
2026-05-18 15:19:41 +0700

    feat: Update port

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,7 +5,7 @@ export const config = {
   port: parseInt(process.env.PORT || "3000"),
   db: {
     host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "5432"),
+    port: parseInt(process.env.DB_PORT || "5434"),
     user: process.env.DB_USER || "postgres",
     password: process.env.DB_PASSWORD || "postgres",
     database: process.env.DB_NAME || "taskflow",
